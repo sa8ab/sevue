884309597a38c2bc05f4ae96fbc39acf90ba2e70
sa8ab <bwsaeed8@gmail.com>
2023-03-21 11:09:59 +0100

    + button docs

---
+++ docs/components/content/button/ButtonRouting.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="items">
+    <RButton to="/">To Home Page</RButton>
+    <RButton href="/">Anchor Tag</RButton>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.items{
+  display: flex;
+  gap: var(--space-1);
+}
+</style>
