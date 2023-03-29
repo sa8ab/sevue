675ffaf349673bbf4b53a3d63851bd0788254129
sa8ab <bwsaeed8@gmail.com>
2023-03-29 19:57:19 +0200

    + [docs] pagination docs

---
+++ docs/components/content/Pagination/PaginationCompact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="doc-item">
+    <RPagination :count="100" v-model="page" bordered compact />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const page = ref(40);
+</script>
+
+<style scoped lang="scss">
+.doc-item {
+  max-width: 100%;
+}
+</style>
---
+++ docs/components/content/Pagination/PaginationDemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="doc-item">
+    <RPagination :count="10" v-model="page" bordered :sideRange="1" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const page = ref(4);
+</script>
+
+<style scoped lang="scss">
+.doc-item {
+  max-width: 100%;
+}
+</style>
