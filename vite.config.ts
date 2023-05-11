611a46e9626318aa9f0d9ef8a34783812ab3f639
sa8ab <bwsaeed8@gmail.com>
2023-05-12 02:32:21 +0330

    + add exports named

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ["vue"],
-      output: { globals: { vue: "Vue" } },
+      output: { globals: { vue: "Vue" }, exports: "named" },
     },
   },
   resolve: {
