5f8aadd0fdf33fe0f72961133d8b1da7b3336a20
sa8ab <bwsaeed8@gmail.com>
2023-08-12 17:33:35 +0330

    add Alert Component

--- dev/pages/AlertPage.vue
+++ dev/pages/AlertPage.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="center-it">
-    <RAlert />
+    <RAlert title="New Alert some text inside it please notice it">
+      <template #icon> icon </template>
+      <template #text>
+        <div>some text</div>
+        <div>some text</div>
+      </template>
+
+      <template #close="{ close }">
+        <div @click="close">Custom Close</div>
+      </template>
+    </RAlert>
+    <RAlert title="New Alert some text inside it please notice it" color="yellow" />
+    <RAlert title="New Alert some text inside it please notice it" color="red" />
   </div>
 </template>
 
--- src/components/alert/RAlert.vue
+++ src/components/alert/RAlert.vue
@@ -1,15 +1,79 @@
 <template>
-  <div class="r-alert">ALERT</div>
+  <div
+    :class="['r-alert']"
+    :style="{
+      '--r-color': color || 'var(--r-prm)',
+    }"
+    v-if="state.active"
+  >
+    <slot name="icon" />
+    <div class="r-alert-main">
+      <div class="r-alert-title">
+        <slot name="title">
+          {{ title }}
+        </slot>
+      </div>
+      <div class="r-alert-text">
+        <slot name="text">
+          {{ text }}
+        </slot>
+      </div>
+    </div>
+    <div class="r-alert-close" v-if="!noClose">
+      <slot name="close" :close="close">
+        <RButton iconOnly textStyle compact round @click="close">
+          <SevueIcon name="close" width="18px" height="18px" />
+        </RButton>
+      </slot>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import { useColor, RButton } from "@/main";
+import { toRef } from "vue";
+import SevueIcon from "../icons/SevueIcon.vue";
+import { reactive } from "vue";
+
 export interface Props {
   title?: string | number | null;
-  description?: string | number | null;
+  text?: string | number | null;
   noClose?: boolean;
   color?: string;
 }
 const props = withDefaults(defineProps<Props>(), {});
+
+const emit = defineEmits<{
+  (e: "close"): void;
+}>();
+
+const color = useColor(toRef(props, "color"));
+
+const state = reactive({
+  active: true,
+});
+
+const close = () => {
+  state.active = false;
+  emit("close");
+};
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.r-alert {
+  display: flex;
+  align-items: center;
+  gap: var(--r-space-1);
+  padding: var(--r-normal-padding);
+  border-radius: var(--r-radius);
+  background: color(color, var(--r-light-alpha));
+  color: color();
+  position: relative;
+  &-title {
+    font-weight: 500;
+  }
+  &-close {
+    align-self: flex-start;
+  }
+}
+</style>
