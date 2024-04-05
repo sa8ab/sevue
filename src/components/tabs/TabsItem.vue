2ef054def6cf79c6d9b1ad71394ac6fe9fc14778
sa8ab <bwsaeed8@gmail.com>
2024-04-05 20:06:38 +0330

    init tabs root and tabs item

---
+++ src/components/tabs/TabsItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { type PrimitiveProps, Primitive } from "@/components/primitive";
+import { RovingFocusItem } from "@/components/roving-focus";
+import { useForwardRef } from "@/composables/useForwardRef";
+
+export interface TabsRootProps extends PrimitiveProps {}
+
+// export type TabsRootEmits = {
+// };
+
+const props = withDefaults(defineProps<PrimitiveProps>(), {});
+
+// const emit = defineEmits<TabsRootEmits>();
+
+const { forwardRef } = useForwardRef();
+</script>
+
+<template>
+  <RovingFocusItem asChild>
+    <Primitive :asChild="asChild" :as="as" :ref="forwardRef">
+      <slot />
+    </Primitive>
+  </RovingFocusItem>
+</template>
---
+++ src/components/tabs/TabsRoot.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+export const [injectTabsRootContext, provideTabsRootContext] = useContext<TabsRootContext>("TabsRoot");
+</script>
+
+<script setup lang="ts">
+import { type PrimitiveProps, Primitive } from "@/components/primitive";
+import { RovingFocusRoot } from "@/components/roving-focus";
+import { useContext } from "@/composables/useContext";
+import { useForwardRef } from "@/composables/useForwardRef";
+
+import { type Orientation, Dir } from "@/types";
+import { type Ref, toRefs } from "vue";
+
+export interface TabsRootProps extends PrimitiveProps {
+  initialValue?: number | string;
+  orientation?: Orientation;
+  dir?: Dir;
+}
+
+export type TabsRootEmits = {
+  change: [];
+  "update:modelValue": [];
+};
+
+export interface TabsRootContext {
+  modelValue: Ref<string | number | undefined>;
+  setModelValue: (v: string | number | undefined) => void;
+  orientation: Ref<Orientation | undefined>;
+  dir: Ref<Dir | undefined>;
+}
+
+const props = withDefaults(defineProps<TabsRootProps>(), {});
+
+const emit = defineEmits<TabsRootEmits>();
+
+const model = defineModel<string | number>({
+  required: false,
+});
+
+const { forwardRef } = useForwardRef();
+
+const { dir, orientation } = toRefs(props);
+
+provideTabsRootContext({
+  dir,
+  orientation,
+  modelValue: model,
+  setModelValue: (v) => (model.value = v),
+});
+</script>
+
+<template>
+  <RovingFocusRoot asChild :orientation="orientation">
+    <Primitive :asChild="asChild" :as="as" :ref="forwardRef">
+      <slot />
+    </Primitive>
+  </RovingFocusRoot>
+</template>
