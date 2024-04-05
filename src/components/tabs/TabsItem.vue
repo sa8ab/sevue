d785b1f365cc2079c3daf81c293bee9ac90d171f
sa8ab <bwsaeed8@gmail.com>
2024-04-05 20:10:43 +0330

    update tabsitem

--- src/components/tabs/TabsItem.vue
+++ src/components/tabs/TabsItem.vue
@@ -2,22 +2,40 @@
 import { type PrimitiveProps, Primitive } from "@/components/primitive";
 import { RovingFocusItem } from "@/components/roving-focus";
 import { useForwardRef } from "@/composables/useForwardRef";
+import { injectTabsRootContext } from "./TabsRoot.vue";
+import { computed } from "vue";
 
-export interface TabsRootProps extends PrimitiveProps {}
+export interface TabsItemProps extends PrimitiveProps {
+  value: string | number;
+  disabled?: boolean;
+}
 
 // export type TabsRootEmits = {
 // };
 
-const props = withDefaults(defineProps<PrimitiveProps>(), {});
+const props = withDefaults(defineProps<TabsItemProps>(), {});
 
 // const emit = defineEmits<TabsRootEmits>();
 
 const { forwardRef } = useForwardRef();
+
+const rootContext = injectTabsRootContext();
+
+const isSelected = computed(() => rootContext.modelValue.value === props.value);
+
+const tabindex = computed(() => (isSelected.value ? "0" : "-1"));
 </script>
 
 <template>
   <RovingFocusItem asChild>
-    <Primitive :asChild="asChild" :as="as" :ref="forwardRef">
+    <Primitive
+      :asChild="asChild"
+      :as="as"
+      :ref="forwardRef"
+      :tabindex="tabindex"
+      :disabled="disabled"
+      :data-disabled="disabled"
+    >
       <slot />
     </Primitive>
   </RovingFocusItem>
